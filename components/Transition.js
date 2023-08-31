b9ffe7e3eba857b4c34e7a6b5a79c014523e000f
Kirill Lyashenko <tltelf@yandex.ru>
2023-08-31 23:50:42 +0400

    add: Home

--- components/Transition.js
+++ components/Transition.js
@@ -3,15 +3,15 @@ import { motion } from 'framer-motion';
 const transitionVariants = {
   initial: {
     x: '100%',
-    width: '100%'
+    opacity: 1,
   },
   animate: {
-    x: '0%',
-    width: '0%'
+    x: '100%',
+    opacity: 0,
   },
   exit: {
-    x: ['0%', '100%'],
-    width: ['0%', '100%'],
+    x: '100%',
+    opacity: 1,
   }
 }
 
@@ -21,7 +21,6 @@ const Transition = () => {
       <motion.div className='fixed top-0 bottom-0 right-full w-screen h-screen z-30 bg-[#2e2257]'
         variants={transitionVariants} initial='initial' animate='animate' exit='exit' transition={{delay: 0.2, duration: 0.6, ease: 'easeInOut'}}
       >
-        1
       </motion.div>
     </>
   );
